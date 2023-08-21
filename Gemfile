960c0a17c1f1284c05d15b8d66d954a542814de3
Mehroze Murtaza <mehroze.murtaza@devsinc.com>
2023-08-21 13:28:57 -0700

    created actions/templates for model book

--- Gemfile
+++ Gemfile
@@ -73,3 +73,4 @@ end
 
 gem 'pg'
 gem 'dotenv-rails'
+gem 'byebug'
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,22 @@
 class BooksController < ApplicationController
   def index
-    @books=Book.all
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(params.require(:book).permit(:name))
+    if @book.save
+      redirect_to @book
+    else
+      render action: 'new'
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 end
